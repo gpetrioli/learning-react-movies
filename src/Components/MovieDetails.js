e75fa4e7fc3af21fe449398043923b6cf6ac7e7a
Gabriele Petrioli <gpetrioli@users.noreply.github.com>
2018-02-01 14:05:04 +0200

    update to movie details

--- src/Components/MovieDetails.js
+++ src/Components/MovieDetails.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import {Redirect} from 'react-router-dom';
+import {Redirect, Link} from 'react-router-dom';
+import styles from './MovieDetails.module.css';
 
 
 export default class MovieDetails extends Component{
@@ -26,14 +27,26 @@ export default class MovieDetails extends Component{
     
     render(){
         const movie = this.state.details,
-              backdrop = `${this.props.imagepath}w1280${movie.backdrop_path}`;
+              backdrop = `${this.props.imagepath}w1280${movie.backdrop_path}`,
+              poster = `${this.props.imagepath}w780${movie.poster_path}`;
+        
         if (this.state.fail){
             return <Redirect to="/movies/genre" push={true} />
         }
+        
         document.documentElement.style.setProperty('--backdrop',`url('${backdrop}')`);
         return (
-            <div>
-                <h1>{movie.original_title}</h1>
+            <div className="container-fluid">
+                <h1 className="text-center">{movie.original_title}</h1>
+                <div className="row">
+                    <div className="col-3 offset-1"><img className={`img-fluid ${styles.poster}`} src={poster}></img></div>
+                    <div className="col-7">
+                        <div className={`${styles.text} ${styles['text-summary']}`}>{movie.overview}</div>
+                        <div>{(movie.genres||[]).map(({name,id})=>(
+                                <Link to={`/movies/genre/${id}`} className={styles.genre}>{name}</Link>
+                            ))}</div>
+                    </div>
+                </div>
             </div>
         )
     }
