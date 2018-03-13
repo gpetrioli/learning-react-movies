b195520439b4542b1588423bee795b56b47259b4
Gabriele Petrioli <gpetrioli@users.noreply.github.com>
2018-03-13 11:49:35 +0200

    Start using HashRouter (instead of BrowserRouter) to allow hosting on github pages

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {BrowserRouter} from 'react-router-dom';
+import {HashRouter} from 'react-router-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
@@ -33,8 +33,8 @@ const store = createStore(movieApp, /* preloadedState, */ composeEnhancers(
 
 ReactDOM.render((
     <Provider store={store}>
-        <BrowserRouter>
+        <HashRouter basename="/">
             <App/>
-        </BrowserRouter>
+        </HashRouter>
     </Provider>), document.getElementById('root'));
 registerServiceWorker();
